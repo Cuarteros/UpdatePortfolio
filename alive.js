b65619b37caf229265007ee34328ef05ed58b86c
Cuarteros <bhingcuarteros@gmail.com>
2020-03-12 12:50:31 -0700

    5th Update

--- alive.js
+++ alive.js
@@ -1,1 +1,10 @@
 "use strict"
+
+
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput").value;
+    
+    // Displaying the value
+    alert(inputVal);
+}
